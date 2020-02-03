26dbaab3b45a003707c4bbdd38c6c15077ad073c
Jonathan Rotter <hydrogen31415@gmail.com>
2020-02-02 20:08:59 -0600

    time check

--- Config.ts
+++ Config.ts
@@ -11,3 +11,11 @@ export class Config {
 
     private constructor() {}
 }
+
+// time spent check
+// Coding: 86 min + 225 min + 230 min + 146 min + 86 min = 773 min
+// Stackoverflow: 36 min
+// GitHub: 31 min + 15 min
+//
+// Total: 855 min = 14h 15min
+// As of the commit that added this
